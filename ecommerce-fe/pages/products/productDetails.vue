371d8855d154ba4d165be3c34d8b2023cee4ba6a
Huỳnh Văn Ngoãn <91590350+huynhvanngoan@users.noreply.github.com>
2024-08-28 17:51:12 +0700

    Update dev-backend with only backend code

--- ecommerce-fe/pages/products/productDetails.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div>
-        <h1>This is product details page</h1>
-    </div>
-</template>
-
-<script setup></script>
-
-<style lang="scss" scoped></style>
--- ecommerce-fe/router/middleware/vendor.ts
+++
@@ -1,6 +0,0 @@
-export default defineNuxtRouteMiddleware((to, from) => {
-    const { $auth } = useNuxtApp()
-    if (!$auth.loggedIn || $auth.user.role !== 'vendor') {
-      return navigateTo('/')
-    }
-  })
